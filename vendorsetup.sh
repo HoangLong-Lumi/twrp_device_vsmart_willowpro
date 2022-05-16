b1ca31dc8e39513db1647c19137fadc6f9ba18f7
long266 <71440530+long266@users.noreply.github.com>
2022-05-16 17:09:52 +0700

    willowpro : fix typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -70,7 +70,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 		#R11
 		export FOX_R11=1
-		export FOX_BUILD_TYPE=	
+		export FOX_BUILD_TYPE=Stable
 		export OF_MAINTAINER=Long266
 		export FOX_VERSION="R11.1"
 
